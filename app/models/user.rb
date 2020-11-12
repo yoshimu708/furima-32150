9d09a21d8696d7fd91c3dd291bda35ab27a9c250
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-12 15:43:51 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email,    unipueness: true
-  with_options presence: true,confirmation: true, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: '半角数字混合で入力してください' } do
-    validates :password,
-  end
+  validates :email,    uniqueness: true
+  validates :password,presence: true,confirmation: true, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: '半角数字混合で入力してください' }
+  
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
+
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角(カタカナ)を使用してください' } do
     validates :first_name_reading
     validates :last_name_reading
   end
+
   validates :birthday, presence: true
 
 end
