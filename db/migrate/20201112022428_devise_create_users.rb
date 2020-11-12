6b9c0c070917316de60e41878c0a471b770c2191
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-12 11:38:25 +0900

    ユーザーテーブルの作成

--- db/migrate/20201112022428_devise_create_users.rb
+++ db/migrate/20201112022428_devise_create_users.rb
@@ -11,6 +11,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      ##自分で記入
+      t.string :nickname,              null:false
+      t.string :first_name,            null:false
+      t.string :last_name,             null:false
+      t.string :first_name_reading,    null:false
+      t.string :last_name_reading,     null:false
+      t.date   :birtyday,              null:false
+
       ## Rememberable
       t.datetime :remember_created_at
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,12 @@ ActiveRecord::Schema.define(version: 2020_11_12_022428) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_reading", null: false
+    t.string "last_name_reading", null: false
+    t.date "birtyday", null: false
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
