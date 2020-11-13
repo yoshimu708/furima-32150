34f9fc554f613e35e701b5e8819808d5e4d477a4
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-13 15:12:29 +0900

    Active Strageの導入

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ end
 gem 'devise'
 
 gem 'pry-rails'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:items).permit(:content,:image).merge(user_id: current_user.id)
+  end
+  
 end
