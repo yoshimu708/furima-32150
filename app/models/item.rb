1eca86f1be44efd87541a268a890ec6a4e1e1f60
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-17 19:24:48 +0900

    ユーザーの表示変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to :user
+  
   belongs_to :category
   belongs_to :status
   belongs_to :shipmentSource
