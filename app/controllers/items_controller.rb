16312fb2b8657f36c5e19caa6a1c2a36959a4872
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-18 14:47:08 +0900

    商品消去機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update,:destroy]
 
   def index
     @items = Item.all
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+  
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
