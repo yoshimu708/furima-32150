702806cd3aaff10226be2df5d6187efe4bcffe63
yoshimu708 <naoyasoccer0928@gmail.com>
2020-11-23 18:47:11 +0900

    画像機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,8 @@ class MessagesController < ApplicationController
   def create
    @message = Message.new(message_params)
     if @message.save
-      redirect_to item_path(@message.item) 
-    else
-      @message = @message.item
-      @messages = @item.messages
-      render item_path
-    end
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end 
   end
 
   private
